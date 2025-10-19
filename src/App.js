2375697753401a815e8c82c85846fb5fc53e316e
Vie-V <avellantozza@gmail.com>
2025-10-19 17:28:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
 
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome to CampusPlug 🎓</Text>
+    </View>
+  );
+}
