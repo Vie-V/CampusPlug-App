0b96bb76c6fbd3e50d6cc00d85e7f5dbe84883f7
Vie-V <avellantozza@gmail.com>
2025-10-19 17:48:03 +0200

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Header() {
+  return (
+    <View style={{ padding: 20, backgroundColor: '#007AFF' }}>
+      <Text style={{ color: 'white', fontSize: 20 }}>CampusPlug Header</Text>
+    </View>
+  );
+}
